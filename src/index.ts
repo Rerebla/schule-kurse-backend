8e04f8c295262edfdae971fb21f2619178d966d6
Rerebla <52045338+Rerebla@users.noreply.github.com>
2021-11-14 18:42:21 +0100

    Add /request route and setup cors.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 // import kursbuch from './kursbuch.json';
 require('dotenv').config();
 const app = express();
 const config = process.env;
 const port = config.PORT;
-
-
+const corsOptions: cors.CorsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+interface requestBody {
+    firstName: string;
+    lastName: string;
+    select1SS: string;
+    select1WS: string;
+    select2SS: string;
+    select2WS: string;
+}
+app.post("/request", (req: Request, res: Response) => {
+    const body: requestBody = req.body;
+    console.log(body);
+    res.sendStatus(200);
+});
 
 
 app.listen(port, () => {
